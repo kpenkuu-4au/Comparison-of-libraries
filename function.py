1f51419ffef8ddf7849a5421d06fa040448648bc
kpenkuu-4au <revolva47@gmail.com>
2024-12-24 10:39:51 +0500

    to Git

--- function.py
+++ function.py
@@ -1,24 +1,65 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt                               #Импорт библиотек
+import numpy as np
+
+"""
+Функция rotation_3d производит вращение 3д графика в 2 режимах:
+1 режим - вращение относительно оси Z;
+2 режим - вращение относительно 2 других осей.
+За режим вращения отвечает параметр "mode"
+"""
 
 
-def rotation_3d(ax):
-    if ax:
-        for angle in range(0, 360 * 4 + 1):
+def rotation_3d(ax, mode):
+    if mode == 1:
+        for angle in range(0, 360 + 1):
+            angle_norm = (angle + 180) % 360 - 270
+            rotation1 = 0
+            if angle <= 360:
+                rotation1 = angle_norm
+            ax.view_init(azim=rotation1)
+            plt.draw()
+            plt.pause(.001)
+    if mode == 2:
+        for angle in range(0, 360 * 3 + 1):
             angle_norm = (angle + 180) % 360 - 180
-            rotation1 = rotation2 = rotation3 = 0
+            rotation1 = rotation2 = 0
             if angle <= 360:
                 rotation1 = angle_norm
             elif angle <= 360 * 2:
-                plt.axis('off')
                 rotation2 = angle_norm
-            elif angle <= 360 * 3:
-                plt.axis('on')
-                rotation3 = angle_norm
             else:
-                plt.axis('off')
-                rotation1 = rotation2 = rotation3 = angle_norm
-            ax.view_init(rotation1, rotation2, rotation3)
+                rotation1 = rotation2 = angle_norm
+            ax.view_init(elev=rotation1, roll=rotation2)
             plt.draw()
             plt.pause(.001)
-    else:
-        raise TypeError('Object is not axis')
+
+
+"""
+Функция "random_walk" рассчитывает логику произвольного 
+построения линии на графике. Принимает аргумент "steps" 
+(количество шагов отрисовки). Переменная "start_pos" задает координату 
+старта отрисовки. Переменная "max_step" отвечает за величину отклонения 
+координат для отрисовки линии (в случайном порядке). Функция возвращает
+список "walk"
+"""
+
+
+def random_walk(steps):
+    start_pos = (0.5, 0.5, 0.5)
+    max_step = 0.03
+    step = np.random.uniform(-max_step, max_step, size=(steps, 3))
+    walk = start_pos + np.cumsum(step, axis=0)
+    return walk
+
+
+"""
+Функция "update_lines" производит обновление отрисовки линий на графике.
+Принимает аргументы "num", "walks", "lines". Возвращает измененный 
+список "lines"
+"""
+
+
+def update_lines(num, walks, lines):
+    for line, walk in zip(lines, walks):
+        line.set_data_3d(walk[:num, :].T)
+    return lines
--- matplotlib_items.py
+++ matplotlib_items.py
@@ -1,69 +1,75 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-from function import rotation_3d
+import matplotlib.pyplot as plt                                     #Импорт библиотек
+import matplotlib.animation as animation
+import function as f
+import data_read as r
 
-df = pd.read_csv('data/user_behavior_dataset.csv')
-colors = [
-    'darkturquoise', 'khaki', 'orangered', 'yellowgreen', 'gold',
-    'olivedrab', 'mediumspringgreen', 'deepskyblue', 'crimson', 'yellow'
-]
-users = df['User ID']
-device = df['Device Model']
-OS = df['Operating System']
-app_us = df['App Usage Time (min/day)']
-screen = df['Screen On Time (hours/day)']
-battery = df['Battery Drain (mAh/day)']
-num_app = df['Number of Apps Installed']
-data_us = df['Data Usage (MB/day)']
-age = df['Age']
-gender = df['Gender']
-UBC = df['User Behavior Class']
 
-# fig1 = plt.figure(figsize=(8, 5), dpi=80)
-# plt.bar(
-#     device[:10],
-#     data_us[:10],
-#     color='deepskyblue',
-#     width=0.58,
-# )
-# plt.title('Using data in models', fontsize=16)
-# plt.xlabel('Device Model')
-# plt.ylabel('Data Usage (MB/day)')
-# plt.show()
-#
-# fig2 = plt.figure(figsize=(10, 4), dpi=80)
-# plt.plot(users[:10], age[:10], color='lime')
-# plt.scatter(users[:10], age[:10], color='magenta')
-# plt.title('Age of users', fontsize=16)
-# plt.xlabel('User ID')
-# plt.ylabel('Age')
-# plt.show()
-#
-# fig3, ax1 = plt.subplots(figsize=(9, 7), dpi=80)
-# ax1.pie(
-#     data_us[:3],
-#     colors=colors,
-#     autopct='%1.1f%%',
-#     pctdistance=0.67,
-#     explode=(0, 0, 0.25),
-#     radius=0.7
-#)
-# ax1.legend(device[:3], title='Device Model', loc="lower left")
-# plt.title('Using data in models', fontsize=16)
-# plt.show()
+fig1 = plt.figure(figsize=(8, 5), dpi=80)
+plt.bar(
+    r.device[:10],
+    r.data_us[:10],
+    color='deepskyblue',
+    width=0.58,
+)
+plt.title('Using data in models', fontsize=16)
+plt.xlabel('Device Model')
+plt.ylabel('Data Usage (MB/day)')
+plt.show()
 
-# fig4 = plt.scatter(age, screen, c=age, alpha=0.58)
-# plt.xlabel('Age')
-# plt.ylabel('Screen On Time (hours/day)')
-# plt.title('Screen Time by Age', fontsize=16)
-# plt.show()
+fig2 = plt.figure(figsize=(10, 4), dpi=80)
+plt.plot(r.users[:10], r.age[:10], color='lime')
+plt.scatter(r.users[:10], r.age[:10], color='magenta')
+plt.title('Age of users', fontsize=16)
+plt.xlabel('User ID')
+plt.ylabel('Age')
+plt.show()
 
-fig5 = plt.figure(figsize=(8, 7), dpi=100)
+fig3, ax1 = plt.subplots(figsize=(9, 7), dpi=80)
+ax1.pie(
+    r.data_us[:3],
+    colors=r.colors,
+    autopct='%1.1f%%',
+    pctdistance=0.67,
+    explode=(0, 0, 0.25),
+    radius=0.7
+)
+ax1.legend(r.device[:3], title='Device Model', loc="lower left")
+plt.title('Using data in models', fontsize=16)
+plt.show()
+
+fig4 = plt.scatter(r.age, r.users, c=r.age, alpha=0.58, cmap='cividis')
+plt.xlabel('Age')
+plt.ylabel('User ID')
+plt.title('User ID by Age', fontsize=16)
+plt.show()
+
+fig5 = plt.figure(figsize=(7, 6), dpi=120)
 ax2 = fig5.add_subplot(projection='3d')
-ax2.scatter(age, UBC, users, c=age, alpha=0.58)
-ax2.set_xlabel('Age')
-ax2.set_ylabel('User Behavior Class')
+ax2.scatter(r.UBC, r.age, r.users, c=r.age, alpha=0.58, cmap='cividis')
+ax2.set_xlabel('User Behavior Class')
+ax2.set_ylabel('Age')
 ax2.set_zlabel('User ID')
 plt.title('User Behavior Class by Age', fontsize=16)
-rotation_3d(ax2)
+f.rotation_3d(ax2, 1)
+plt.show()
 
+fig6 = plt.figure(figsize=(7, 6), dpi=120)                    #Создание изображения
+ax3 = fig6.add_subplot(projection="3d")                   #Создание графика 3д-осей
+steps = 1000                                                         #Количество шагов отрисовки
+walks = [f.random_walk(steps) for index in range(4)]  #Создание списка количества линий
+lines = [ax3.plot([], [], [])[0] for _ in walks]     #Создание списка координат линий
+ax3.set(xlim3d=(0, 1))                                #Задание предела для осей координат
+ax3.set(ylim3d=(0, 1))
+ax3.set(zlim3d=(0, 1))
+ax3.set_facecolor('dimgray')                       #Настройка цвета фона графика осей
+plt.gcf().set_facecolor('dimgray')                 #Настройка цвета фона изображения
+ani = animation.FuncAnimation(                 #Настраиваем объект анимации:
+    fig6,                                        #Изображение графика, где отображается анимация
+    f.update_lines,                         #Функция, импорт из модуля function
+    steps,                                      #Переменная
+    fargs=(walks, lines),                  #Списки параметров для функции update_lines
+    repeat=False,                            #Зацикливать ли анимацию
+    interval=10                               #Задержка между кадрами в мс
+)
+plt.axis('off')                                  #Отключаем график с осями (для лучшего восприятия)
+f.rotation_3d(ax3, 2)            #Запуск функции
